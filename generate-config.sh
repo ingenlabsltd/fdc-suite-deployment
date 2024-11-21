da3e8d97ff2f5f441c2033504737fbab85cd50a4
filipdf <filip@kravapolde.com>
2024-11-21 13:48:07 +0100

    add config generating script

---
+++ generate-config.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu
+
+source <(grep -v '^#' "./.env" | sed -E 's|^(.+)=(.*)$|: ${\1=\2}; export \1|g')
+
+config_files=(
+    "verifiers/btc/database.env"
+    "verifiers/btc/indexer.env"
+    "verifiers/btc/verifier.env"
+    "verifiers/doge/database.env"
+    "verifiers/doge/indexer.env"
+    "verifiers/doge/verifier.env"
+    "verifiers/xrp/database.env"
+    "verifiers/xrp/config.toml"
+    "verifiers/xrp/verifier.env"
+)
+
+for config_file in "${config_files[@]}"; do
+    echo "writing config file ${config_file}"
+    envsubst < "${config_file}.example" > "${config_file}"
+done
+
+echo "done"
